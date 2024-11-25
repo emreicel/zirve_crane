b60ba8a7af90fcd569d81c38aa5c90f91fd96a22
Emre Icel <emreicel@192.168.1.47>
2024-11-25 22:09:28 +0300

    makyajlama 4

--- app/helpers/payment_methods_helper.rb
+++ app/helpers/payment_methods_helper.rb
@@ -1,2 +1,20 @@
 module PaymentMethodsHelper
-end
+    def render_payment_method_icon(method)
+      icon_class = case method.downcase
+                  when /nakit/, /cash/
+                    'fa-money-bill-wave'
+                  when /kredi/, /credit/
+                    'fa-credit-card'
+                  when /havale/, /eft/
+                    'fa-university'
+                  when /çek/, /check/
+                    'fa-money-check'
+                  else
+                    'fa-wallet'
+                  end
+  
+      content_tag :div, class: 'payment-method-icon' do
+        content_tag :i, nil, class: "fas #{icon_class}"
+      end
+    end
+  end
