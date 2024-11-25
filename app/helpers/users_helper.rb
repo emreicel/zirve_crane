b802c0899fdbce50295f0634771b17368ba0f9e1
Emre Icel <emreicel@192.168.1.47>
2024-11-25 21:21:44 +0300

    makyajlama

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
-end
+    def render_user_role_badge(role)
+      badge_class = case role.to_s.downcase
+      when 'admin', 'yönetici'
+        'text-bg-danger'
+      when 'manager', 'müdür'
+        'text-bg-warning'
+      when 'user', 'kullanıcı'
+        'text-bg-info'
+      else
+        'text-bg-secondary'
+      end
+  
+      tag.span(role.to_s.titleize, class: "badge rounded-pill #{badge_class}")
+    end
+  end
