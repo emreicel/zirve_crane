89098dc2022af6b088cf1e89567d03cd396a9761
Emre Icel <emreicel@192.168.1.47>
2024-11-27 00:02:30 +0300

    ufak düzenlemeler yapıldı

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,3 +72,84 @@ default_roles.each do |role|
     r.description = role[:description]
   end
 end
+
+# ... mevcut vinç ve rol kodları ...
+
+# Varsayılan kullanıcıları oluştur
+default_users = [
+  {
+    name: 'Sistem Yöneticisi',
+    email: 'emreicel@gmail.com',
+    password: '12345678',
+    password_confirmation: '12345678',
+    role: Role.find_by(name: 'super_admin')
+  },
+  {
+    name: 'Muhasebeci',
+    email: 'muhasebe@zirve.com',
+    password: 'Zirve123!',
+    password_confirmation: 'Zirve123!',
+    role: Role.find_by(name: 'accountant')
+  },
+  {
+    name: 'Normal Kullanıcı',
+    email: 'kullanici@zirve.com',
+    password: 'Zirve123!',
+    password_confirmation: 'Zirve123!',
+    role: Role.find_by(name: 'user')
+  }
+]
+
+default_users.each do |user_data|
+  user = User.find_or_initialize_by(email: user_data[:email])
+  if user.new_record?
+    user.assign_attributes(
+      name: user_data[:name],
+      password: user_data[:password],
+      password_confirmation: user_data[:password_confirmation],
+      role: user_data[:role]
+    )
+    user.save!
+    puts "#{user.name} kullanıcısı oluşturuldu"
+  end
+end
+
+puts "#{User.count} adet kullanıcı mevcut"
+
+crane_companies = [
+  {
+    name: 'Zirve Vinç',
+    address: 'Ankara Organize Sanayi Bölgesi 1234. Cadde No: 56',
+    phone: '0312 555 44 33',
+    email: 'info@zirvevinc.com',
+    tax_office: 'Ankara OSB',
+    tax_number: '1234567890',
+    description: 'Türkiye\'nin önde gelen vinç kiralama şirketi'
+  },
+  {
+    name: 'Anadolu Vinç',
+    address: 'İstanbul Dudullu OSB Mahallesi 5. Sokak No: 23',
+    phone: '0216 444 55 66',
+    email: 'iletisim@anadoluvinc.com',
+    tax_office: 'Ümraniye',
+    tax_number: '9876543210',
+    description: 'Endüstriyel vinç çözümleri'
+  },
+  {
+    name: 'Mega Vinç',
+    address: 'İzmir Atatürk OSB 10001 Sokak No: 45',
+    phone: '0232 333 22 11',
+    email: 'bilgi@megavinc.com',
+    tax_office: 'İzmir Konak',
+    tax_number: '5678901234',
+    description: 'Profesyonel vinç hizmetleri'
+  }
+]
+
+crane_companies.each do |company_data|
+  CraneCompany.find_or_create_by!(tax_number: company_data[:tax_number]) do |company|
+    company.attributes = company_data
+    puts "#{company.name} firması oluşturuldu"
+  end
+end
+
